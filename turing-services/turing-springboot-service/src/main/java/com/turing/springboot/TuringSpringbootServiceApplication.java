cb8a395333b68cc787494f73105795bda065f6a5
Tony <economic_luojie@outlook.com>
2023-12-13 15:16:12 +0800

    fix: add module turing-springboot-service

---
+++ turing-services/turing-springboot-service/src/main/java/com/turing/springboot/TuringSpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.turing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringSpringbootServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringSpringbootServiceApplication.class, args);
+    }
+
+}
---
+++ turing-services/turing-springboot-service/src/test/java/com/turing/springboot/TuringSpringbootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turing.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringSpringbootServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
