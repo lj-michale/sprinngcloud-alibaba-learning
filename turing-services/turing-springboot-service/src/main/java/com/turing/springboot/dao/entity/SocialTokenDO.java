c225ea77324c679bc7835c36cc52ecf7476cc2b8
Tony <economic_luojie@outlook.com>
2023-12-15 17:56:20 +0800

    feat: 第三方token

--- turing-services/turing-springboot-service/src/main/java/com/turing/springboot/dao/entity/SocialTokenDO.java
+++ turing-services/turing-springboot-service/src/main/java/com/turing/springboot/dao/entity/SocialTokenDO.java
@@ -11,24 +11,20 @@ import lombok.Data;
  */
 @Data
 @Builder
-//@ApiModel(description = "第三方token")
 public class SocialTokenDO {
     /**
      * 开放id
      */
-//    @ApiModelProperty(value = "开放id")
     private String openId;
 
     /**
      * 访问令牌
      */
-//    @ApiModelProperty(value = "访问令牌")
     private String accessToken;
 
     /**
      * 登录类型
      */
-//    @ApiModelProperty(value = "登录类型")
     private Integer loginType;
 
 }
