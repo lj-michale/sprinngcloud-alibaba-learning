010d27283fe720b2938d39f40fbfdd67bb674c84
Tony <economic_luojie@outlook.com>
2023-12-16 14:25:56 +0800

    feat: add service turing-gateway

---
+++ turing-services/turing-gateway/src/main/java/org/turing/gateway/TuringGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.turing.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringGatewayApplication.class, args);
+    }
+
+}
---
+++ turing-services/turing-gateway/src/test/java/org/turing/gateway/TuringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turing.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
