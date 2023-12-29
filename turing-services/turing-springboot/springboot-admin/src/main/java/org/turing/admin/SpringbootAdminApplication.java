c07115ec49f71506f2a1aaa43c960d3d2cd45758
Tony <economic_luojie@outlook.com>
2023-12-29 10:25:59 +0800

    feat: add springboot admin

---
+++ turing-services/turing-springboot/springboot-admin/src/main/java/org/turing/admin/SpringbootAdminApplication.java
@@ -0,0 +1,13 @@
+package org.turing.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminApplication.class, args);
+    }
+
+}
---
+++ turing-services/turing-springboot/springboot-admin/src/test/java/org/turing/admin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turing.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
